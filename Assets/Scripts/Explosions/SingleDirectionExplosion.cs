f34c0ad19fc99a601805f5e39179288fdc9094d0
JoeShanahan <iam3djoe@gmail.com>
2026-07-23 18:21:44 +0100

    single direction explosion started

--- Assets/Scripts/Explosions/SingleDirectionExplosion.cs
+++ Assets/Scripts/Explosions/SingleDirectionExplosion.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AllDirectionExplosion : ExplosionBase
+public class SingleDirectionExplosion : ExplosionBase
 {
     [Header("Range")]
     [SerializeField] private float _powerfulRange = 2;
     [SerializeField] private float _weakRange = 2;
+    [SerializeField] private float _powerfulWidth = 3;
+    [SerializeField] private float _weakWidth = 5;
     
     [Header("Force")]
     [SerializeField] private float _powerfulForce = 100;
@@ -22,50 +24,59 @@ public class AllDirectionExplosion : ExplosionBase
     [SerializeField]
     private GameObject _particlePrefab;
 
+    private void Start()
+    {
+        GenerateAllSnapDirections();
+    }
     
     public override void Explode(Vector3 position, Vector3 facing)
     {
-        GenerateAllSnapDirections();
-        
-        foreach (Collider col in Physics.OverlapSphere(position, _powerfulRange + _weakRange))
-        {
-            if (col.attachedRigidbody == null)
-                continue;
 
-            Vector3 diff = col.transform.position - position;
-
-            Vector3 bestLaunchVec = GetClosestDirection(diff);
-
-            float lerpAmount = diff.magnitude > _powerfulRange ? _weakLift : _powerfulLift;
-            float force = diff.magnitude > _powerfulRange ? _weakForce : _powerfulForce;
-
-            bestLaunchVec = Vector3.Lerp(bestLaunchVec, Vector3.up, lerpAmount).normalized;
-            col.attachedRigidbody.AddForce(bestLaunchVec * force, ForceMode.Impulse);
-        }
-        
-        GameObject newObj = Instantiate(_particlePrefab, transform.position, Quaternion.identity);
+        GameObject newObj = Instantiate(_particlePrefab, transform.position, Quaternion.LookRotation(facing, Vector3.up));
         Destroy(newObj, 8);
     }
 
+    private Vector3 _cachedDirection;
+    private Vector3 _snappedForward;
+    private Vector3 _snappedRight;
 
     public void OnDrawGizmos()
     {
-        Vector3 littleUp = new Vector3(0, -0.45f, 0);
+        if (transform.forward != _cachedDirection)
+        {
+            _cachedDirection = transform.forward;
+            _snappedForward = GetClosestDirection(_cachedDirection);
+            _snappedRight = GetClosestDirection(transform.right);
+        }
         
+        Vector3 startPos = transform.position + new Vector3(0, -0.45f, 0);
+        Debug.DrawLine(transform.position, transform.position + _snappedForward, Color.green);
+
+        Vector3 pointA = startPos + (_snappedRight * _powerfulWidth * 0.5f);
+        Vector3 pointB = startPos + (_snappedRight * _powerfulWidth * -0.5f);
+        Vector3 pointC = pointA + (_snappedForward * _powerfulRange);
+        Vector3 pointD = pointB + (_snappedForward * _powerfulRange);
+        
+        Debug.DrawLine(pointA, pointB, Color.red);
+        Debug.DrawLine(pointA, pointC, Color.red);
+        Debug.DrawLine(pointB, pointD, Color.red);
+        Debug.DrawLine(pointC, pointD, Color.red);
+
+        /*
         for (int i = 0; i < 32; i++)
         {
             float circA = (Mathf.PI * 2 / 32) * i;
             float circB = (Mathf.PI * 2 / 32) * (i + 1);
-            
+
             Vector3 pointA = new Vector3(Mathf.Cos(circA) * _powerfulRange, 0, Mathf.Sin(circA) * _powerfulRange);
             Vector3 pointB = new Vector3(Mathf.Cos(circB) * _powerfulRange, 0, Mathf.Sin(circB) * _powerfulRange);
-            
+
             Vector3 pointA2 = new Vector3(Mathf.Cos(circA) * (_powerfulRange + _weakRange), 0, Mathf.Sin(circA) * (_powerfulRange + _weakRange));
             Vector3 pointB2 = new Vector3(Mathf.Cos(circB) * (_powerfulRange + _weakRange), 0, Mathf.Sin(circB) * (_powerfulRange + _weakRange));
-            
+
             pointA += transform.position + littleUp;
             pointB += transform.position + littleUp;
-            
+
             pointA2 += transform.position + littleUp;
             pointB2 += transform.position + littleUp;
 
@@ -74,9 +85,10 @@ public class AllDirectionExplosion : ExplosionBase
                 Debug.DrawLine(transform.position + littleUp, pointA, Color.red);
                 Debug.DrawLine(pointA, pointA2, Color.orange);
             }
-            
+
             Debug.DrawLine(pointA, pointB, Color.red);
             Debug.DrawLine(pointA2, pointB2, Color.orange);
         }
+        */
     }
 }
