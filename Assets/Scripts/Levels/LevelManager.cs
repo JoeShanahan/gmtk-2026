86ad58c0756a598a35c3df07f85dd88fc4f22c01
Joe <iam3djoe@gmail.com>
2026-07-24 19:37:57 +0100

    Feature/level loading (#2)
    * level spawning created
    
    * level loading

---
+++ Assets/Scripts/Levels/LevelManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    private LevelData _selectedLevel;
+    private SpawnedLevel _levelInstance;
+    private float _currentSeconds;
+
+    [SerializeField] 
+    private DebugLevelSelectList _debugUi;
+
+    private InputSystem_Actions _input;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _input = new();
+        _input.Enable();
+    }
+    
+    public void InstantiateLevel(LevelData level)
+    {
+        if (_levelInstance != null)
+        {
+            Destroy(_levelInstance.gameObject);
+        }
+        
+        _selectedLevel = level;
+        _currentSeconds = 0;
+        _levelInstance = Instantiate(level.Prefab).GetComponent<SpawnedLevel>();
+        _debugUi?.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _currentSeconds += Time.deltaTime;
+
+        if (_input.GameControl.DebugLevelSelect.WasPressedThisFrame())
+        {
+            _debugUi?.SetActive(!_debugUi.gameObject.activeInHierarchy);
+        }
+    }
+}
