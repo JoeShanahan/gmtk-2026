5c10b5b5ec7fa292f34d83355a65d5f956334f2c
JoeShanahan <iam3djoe@gmail.com>
2026-07-25 16:59:32 +0100

    saving settings from pause menu

--- Assets/Scripts/InGamePauseMenu.cs
+++ Assets/Scripts/InGamePauseMenu.cs
@@ -1,8 +1,16 @@
 using System;
 using DG.Tweening;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+
+public static class SpeedSettings
+{
+    public static float DEFAULT = 0.05f;
+    public static float SLOW = 0.02f;
+    public static float STOPPED = 0.0f;
+}
 
 public class InGamePauseMenu : MonoBehaviour
 {
@@ -11,6 +19,17 @@ public class InGamePauseMenu : MonoBehaviour
     [SerializeField] private GameSettings _settings;
     [SerializeField] private TimeManager _timeMan;
     [SerializeField] private Transform _mainGameUI;
+
+    [Header("Texts")] 
+    [SerializeField] private TextMeshProUGUI _soundOnText;
+    [SerializeField] private TextMeshProUGUI _soundOffText;
+    [SerializeField] private TextMeshProUGUI _musicOnText;
+    [SerializeField] private TextMeshProUGUI _musicOffText;
+    [SerializeField] private TextMeshProUGUI _autoPauseOnText;
+    [SerializeField] private TextMeshProUGUI _autoPauseOffText;
+    [SerializeField] private TextMeshProUGUI _pauseSpeedDefaultText;
+    [SerializeField] private TextMeshProUGUI _pauseSpeedSlowText;
+    [SerializeField] private TextMeshProUGUI _pauseSpeedStoppedText;
     
     private bool _isActive;
     
@@ -25,6 +44,7 @@ public class InGamePauseMenu : MonoBehaviour
 
         if (_isActive)
         {
+            RefreshTexts();
             Time.timeScale = 0;
             EventSystem.current.SetSelectedGameObject(_firstButton.gameObject);
             _grp.interactable = true;
@@ -63,24 +83,56 @@ public class InGamePauseMenu : MonoBehaviour
         ToggleMenu();
     }
 
+    private void RefreshTexts()
+    {
+        _soundOnText.gameObject.SetActive(_settings.SoundEnabled);
+        _soundOffText.gameObject.SetActive(_settings.SoundEnabled == false);
+        _musicOnText.gameObject.SetActive(_settings.MusicEnabled);
+        _musicOffText.gameObject.SetActive(_settings.MusicEnabled == false);
+        _autoPauseOnText.gameObject.SetActive(_settings.AutoPauseOnSwap);
+        _autoPauseOffText.gameObject.SetActive(_settings.AutoPauseOnSwap == false);
+        _pauseSpeedDefaultText.gameObject.SetActive(Mathf.Approximately(_settings.PauseSpeed, SpeedSettings.DEFAULT));
+        _pauseSpeedSlowText.gameObject.SetActive(Mathf.Approximately(_settings.PauseSpeed, SpeedSettings.SLOW));
+        _pauseSpeedStoppedText.gameObject.SetActive(Mathf.Approximately(_settings.PauseSpeed, SpeedSettings.STOPPED));
+    }
+
     public void ButtonPressSound()
     {
-        
+        _settings.SoundEnabled = !_settings.SoundEnabled;
+        _settings.SaveToPrefs();
+        RefreshTexts();
     }
 
     public void ButtonPressMusic()
     {
-        
+        _settings.MusicEnabled = !_settings.MusicEnabled;
+        _settings.SaveToPrefs();
+        RefreshTexts();
     }
 
     public void ButtonPressAutoPause()
     {
-        
+        _settings.AutoPauseOnSwap = !_settings.AutoPauseOnSwap;
+        _settings.SaveToPrefs();
+        RefreshTexts();
     }
 
     public void ButtonPressSpeed()
     {
-        
+        if (Mathf.Approximately(_settings.PauseSpeed, SpeedSettings.DEFAULT))
+        {
+            _settings.PauseSpeed = SpeedSettings.SLOW;
+        }
+        else if (Mathf.Approximately(_settings.PauseSpeed, SpeedSettings.SLOW))
+        {
+            _settings.PauseSpeed = SpeedSettings.STOPPED;
+        }
+        else
+        {
+            _settings.PauseSpeed = SpeedSettings.DEFAULT;
+        }
+        _settings.SaveToPrefs();
+        RefreshTexts();
     }
     
     public void ButtonPressClose()
