1c2265549f6e4b98aa5b947603dacf93b494aa1d
JoeShanahan <iam3djoe@gmail.com>
2026-07-25 11:06:37 +0100

    save data

--- Assets/Scripts/GameSetup.cs
+++ Assets/Scripts/GameSetup.cs
@@ -2,15 +2,12 @@ using UnityEngine;
 
 public class GameSetup : MonoBehaviour
 {
+    [SerializeField] private SaveData _saveData;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Application.targetFrameRate = 60;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _saveData?.LoadFromPrefs();
     }
 }
