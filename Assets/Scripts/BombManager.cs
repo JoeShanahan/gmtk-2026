35a201573a20071651dc3ab0e8cc0ab4b01cd177
JoeShanahan <iam3djoe@gmail.com>
2026-07-25 17:28:01 +0100

    you can swap backwards now

--- Assets/Scripts/BombManager.cs
+++ Assets/Scripts/BombManager.cs
@@ -10,6 +10,7 @@ public class BombManager : MonoBehaviour
     private List<BombCharacter> _allBombs;
 
     private BombCharacter _selectedBomb;
+    [SerializeField]
     private bool _isPaused = true;
 
     public bool IsPaused => _isPaused;
@@ -67,6 +68,10 @@ public class BombManager : MonoBehaviour
         {
             HandleSwap();
         }
+        else if (_input.GameControl.SwapBack.WasPressedThisFrame())
+        {
+            HandleSwap(true);
+        }
 
         if (!_isPaused)
         {
@@ -77,7 +82,7 @@ public class BombManager : MonoBehaviour
         }
     }
 
-    public void HandleSwap()
+    public void HandleSwap(bool back=false)
     {
         if (_allBombs.Count < 2)
             return;
@@ -86,6 +91,12 @@ public class BombManager : MonoBehaviour
 
         int currentIndex = _allBombs.IndexOf(_selectedBomb);
         int nextIndex = (currentIndex + 1) % _allBombs.Count;
+
+        if (back)
+        {
+            nextIndex = currentIndex == 0 ? _allBombs.Count -1 : currentIndex - 1;
+        }
+        
         _selectedBomb.ReleaseControlOf();
 
         _selectedBomb = _allBombs[nextIndex];
