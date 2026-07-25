248e06040b85a7edab53038176f32c36e8d62ca8
JoeShanahan <iam3djoe@gmail.com>
2026-07-25 11:15:35 +0100

    populating level list from main game level set

--- Assets/Scripts/Levels/MainGameLevelSet.cs
+++ Assets/Scripts/Levels/MainGameLevelSet.cs
@@ -5,6 +5,23 @@ using UnityEngine;
 public class MainGameLevelSet : ScriptableObject
 {
     public IReadOnlyList<LevelData> Levels => _levels;
+
+    public int LevelCount => _levels.Count;
+
+    [SerializeField] private SaveData _saveData;
+
+    public LevelData GetLevel(int levelIndex)
+    {
+        LevelData result = _levels[levelIndex];
+        result.MainGameIndex = levelIndex;
+        return result;
+    }
+
+    public int GetNumberOfStars()
+    {
+        // TODO
+        return 0;
+    }
     
     [SerializeField] private List<LevelData> _levels;
 }
--- Assets/Scripts/MainMenu/LevelSelectCell.cs
+++ Assets/Scripts/MainMenu/LevelSelectCell.cs
@@ -6,9 +6,9 @@ public class LevelSelectCell : MonoBehaviour
     [SerializeField] private Transform[] _stars;
     [SerializeField] private TextMeshProUGUI _text;
 
-    public void Init(LevelData levelData, int levelNum)
+    public void Init(LevelData levelData)
     {
-        _text.text = levelNum.ToString();
+        _text.text = (levelData.MainGameIndex + 1).ToString();
 
         foreach (Transform t in _stars)
         {
