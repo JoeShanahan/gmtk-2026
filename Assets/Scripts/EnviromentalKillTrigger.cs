ef7d24ec313fced3d4fbddd5f375d49fae255f23
AK <alt.kreation@gmail.com>
2026-07-26 18:44:37 +0100

    respawn Garlic when it hits environmental kill trigger

--- Assets/Scripts/EnviromentalKillTrigger.cs
+++ Assets/Scripts/EnviromentalKillTrigger.cs
@@ -2,12 +2,23 @@ using UnityEngine;
 
 public class EnviromentalKillTrigger : MonoBehaviour
 {
+    private GarlicManager _garlicManager;
+    
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Trigger Entered");
         if (other.TryGetComponent<BombCharacter>(out var bombCharacter))
         {
             other.GetComponent<BombCharacter>().Explode();
+        }
+
+        if (other.CompareTag("Garlic"))
+        {
+            if (_garlicManager == null)
+            {
+                _garlicManager = FindAnyObjectByType<GarlicManager>();
+            }
+            
+            _garlicManager.RespawnGarlic(other.gameObject);
         }
     }
 }
