0b9e833681aa378350957b5dcebf478c9dbce89d
JoeShanahan <iam3djoe@gmail.com>
2026-07-26 11:46:21 +0100

    sound!

--- Assets/Scripts/Audio/PlayAudioRespectSettings.cs
+++ Assets/Scripts/Audio/PlayAudioRespectSettings.cs
@@ -9,16 +9,23 @@ public class PlayAudioRespectSettings : MonoBehaviour
     private GameSettings _settings;
 
     [SerializeField] private float _volume = 1;
+
+    [SerializeField] private float _minRandomPitch = 0.9f;
+    [SerializeField] private float _maxRandomPitch = 0.9f;
+
+    private float _myPitch;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        _myPitch = Random.Range(_minRandomPitch, _maxRandomPitch);
         _sound.volume = _settings.SoundEnabled ? _volume : 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _sound.pitch = Time.timeScale * _myPitch;
+
     }
 }
